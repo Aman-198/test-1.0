ccb0814c48aa598fa0ef156b032c515a5004387f
Aman Kumar <149381340+Aman-198@users.noreply.github.com>
2025-07-29 18:15:12 +0530

    Update App.jsx

--- trail/src/App.jsx
+++ trail/src/App.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegCopy } from "react-icons/fa6";
 import "./App.css";
 
 const App = () => {
+  const [copied, setCopied] = useState(false);
   const text = "Hello World";
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
-    <div className="center-wrapper">
+    <div className="center-screen">
       <div className="copy-container">
         <label className="label">User ID:</label>
-        <input type="text" readOnly value={text} className="input-field full-width" />
+        <div className="input-copy-wrapper">
+          <input type="text" readOnly value={text} className="input-field" />
+          <div className="copy-section">
+            {copied && <div className="copied-tooltip">Copied!</div>}
+            <button className="copy-icon" onClick={handleCopy}>
+              <FaRegCopy />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
